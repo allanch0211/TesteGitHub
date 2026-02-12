b51551b2ed404349023daeb9480b63e724d274e9
Allan Christian <allacnh0211@gmail.com>
2026-02-11 21:43:00 -0300

    Adiciona pipeline de testes com Cypress e Allure

--- cypress.config.js
+++ cypress.config.js
@@ -2,31 +2,43 @@ const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
 const {
   addCucumberPreprocessorPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor");
+
 const {
   createEsbuildPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.amazon.com.br", baseUrl: "https://www.saucedemo.com",
+
+    // Base URL dinâmica (escolhe pelo comando)
+    baseUrl: process.env.BASE_URL || "https://www.saucedemo.com",
 
     testIsolation: false,
+
+    // ✅ Configuração do Allure
+    env: {
+      allure: true,
+      allureResultsPath: "allure-results",
+    },
+
     viewportWidth: 1440,
     viewportHeight: 900,
 
     specPattern: "cypress/e2e/features/**/*.feature",
 
     async setupNodeEvents(on, config) {
+
       // Allure
       allureWriter(on, config);
 
       // Cucumber
       await addCucumberPreprocessorPlugin(on, config);
 
-      // Preprocessor para .feature
+      // Preprocessor
       on(
         "file:preprocessor",
         createBundler({
